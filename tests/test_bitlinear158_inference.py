d027a344cc834cb46d30c2d2bf0b2801e3aa8e21
tky823 <tky823.main+github@gmail.com>
2024-12-31 21:23:34 +0900

    Add test of BitLinear158CppInference

--- tests/test_bitlinear158_inference.py
+++ tests/test_bitlinear158_inference.py
@@ -4,6 +4,7 @@ import torch
 
 from bitlinear158compression.bitnet import (
     BitLinear158,
+    BitLinear158CppInference,
     BitLinear158Int8Inference,
     BitLinear158Uint2Inference,
 )
@@ -34,18 +35,24 @@ def main() -> None:
     uint2_inference_model = BitLinear158Uint2Inference.build_from_bitlinear158(
         training_model
     )
+    cpp_inference_model = BitLinear158CppInference.build_from_bitlinear158(
+        training_model
+    )
 
     training_model.to(device)
     int8_inference_model.to(device)
     uint2_inference_model.to(device)
+    cpp_inference_model.to(device)
 
     training_model.eval()
     int8_inference_model.eval()
     uint2_inference_model.eval()
+    cpp_inference_model.eval()
 
     training_elapsed_time = 0
     int8_inference_elapsed_time = 0
     uint2_inference_elapsed_time = 0
+    cpp_inference_elapsed_time = 0
 
     for iter_idx in range(num_iter):
         input = torch.randn((batch_size, in_features))
@@ -54,36 +61,62 @@ def main() -> None:
         with torch.inference_mode():
             start = time.perf_counter()
             training_output = training_model(input)
+
+            if torch.cuda.is_available():
+                torch.cuda.synchronize()
+
             end = time.perf_counter()
             training_elapsed_time += end - start
 
             start = time.perf_counter()
             int8_inference_output = int8_inference_model(input)
+
+            if torch.cuda.is_available():
+                torch.cuda.synchronize()
+
             end = time.perf_counter()
             int8_inference_elapsed_time += end - start
 
             start = time.perf_counter()
             uint2_inference_output = uint2_inference_model(input)
+
+            if torch.cuda.is_available():
+                torch.cuda.synchronize()
+
             end = time.perf_counter()
             uint2_inference_elapsed_time += end - start
+
+            start = time.perf_counter()
+            cpp_inference_output = cpp_inference_model(input)
+
+            if torch.cuda.is_available():
+                torch.cuda.synchronize()
+
+            end = time.perf_counter()
+            cpp_inference_elapsed_time += end - start
 
         if iter_idx == 0:
             assert torch.allclose(training_output, int8_inference_output)
             assert torch.allclose(training_output, uint2_inference_output)
+            assert torch.allclose(training_output, cpp_inference_output, atol=1e-7)
 
             print("Size of model parameters in byte:")
             print(f"\tfloat32: {compute_model_size(training_model):8d}")
             print(f"\t   int8: {compute_model_size(int8_inference_model):8d}")
             print(f"\t  uint2: {compute_model_size(uint2_inference_model):8d}")
+            print(f"\t    cpp: {compute_model_size(cpp_inference_model):8d}")
 
             print("Size of stored state_dict in byte:")
             print(f"\tfloat32: {compute_state_dict_size(training_model):8d}")
             print(f"\t   int8: {compute_state_dict_size(int8_inference_model):8d}")
             print(f"\t  uint2: {compute_state_dict_size(uint2_inference_model):8d}")
+            print(f"\t    cpp: {compute_state_dict_size(cpp_inference_model):8d}")
 
-    print(training_elapsed_time)
-    print(int8_inference_elapsed_time)
-    print(uint2_inference_elapsed_time)
+    print("Total inference time:")
+    print(f"\tfloat32: {training_elapsed_time}")
+    print(f"\t   int8: {int8_inference_elapsed_time}")
+    print(f"\t  uint2: {uint2_inference_elapsed_time}")
+    print(f"\t    cpp: {cpp_inference_elapsed_time}")
 
 
 if __name__ == "__main__":
